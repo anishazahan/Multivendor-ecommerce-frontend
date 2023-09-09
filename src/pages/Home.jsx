3ab94a11c6996039860fed3f6613c5bf26d4f1e1
anishazahan <anishazahan13@gmail.com>
2023-09-09 16:46:22 +0600

    products component design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,22 @@ const Home = () => {
       {/* <div className='py-[45px]'>
                 <FeatureProducts products={products} />
             </div> */}
+      {/* <div className='py-10'>
+                <div className='w-[85%] flex flex-wrap mx-auto'>
+                    <div className="grid w-full grid-cols-3 md-lg:grid-cols-2 md:grid-cols-1 gap-7">
+                        <div className='overflow-hidden'>
+                            <Products title='Latest Product' products={latest_product} />
+                        </div>
+                        <div className='overflow-hidden'>
+                            <Products title='Top Rated Product' products={topRated_product} />
+                        </div>
+                        <div className='overflow-hidden'>
+                            <Products title='Discount Product' products={discount_product} />
+                        </div>
+                    </div>
+                </div>
+            </div> */}
+      <h2>This is footer</h2>
     </>
   );
 };
