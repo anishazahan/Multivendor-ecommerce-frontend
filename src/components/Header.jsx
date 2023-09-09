6ad2120f5162cfcacfa81c4bbdf149f8f1f9df1a
anishazahan <anishazahan13@gmail.com>
2023-09-09 15:22:44 +0600

    header section responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -45,7 +45,7 @@ const Header = () => {
   return (
     <div className="w-full bg-white">
       <div className="header-top bg-[#eeeeee] md-lg:hidden">
-        <div className="w-[85%] lg:w-[90%]  mx-auto">
+        <div className="w-[90%] lg:w-[90%]  mx-auto">
           <div className="flex w-full justify-between items-center h-[50px] text-slate-500">
             <ul className="flex justify-start items-center gap-8">
               <li className="flex relative justify-center items-center gap-2 text-sm after:absolute after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px]">
@@ -109,7 +109,7 @@ const Header = () => {
         </div>
       </div>
       <div className="w-white">
-        <div className="w-[85%] lg:w-[90%] mx-auto">
+        <div className="w-[90%] lg:w-[90%] mx-auto">
           <div className="h-[80px] md-lg:h-[100px] flex justify-between items-center flex-wrap">
             <div className="md-lg:w-full w-3/12 md-lg:pt-4">
               <div className="flex justify-between items-center">
@@ -348,7 +348,7 @@ const Header = () => {
           </div>
         </div>
       </div>
-      <div className="w-[85%] lg:w-[90%] mx-auto">
+      <div className="w-[90%] lg:w-[90%] mx-auto">
         <div className="flex w-full flex-wrap md-lg:gap-8">
           <div className="w-3/12 md-lg:w-full">
             <div className="bg-white relative">
