5c85dca8fefd272938cc20d2bd62567af2f58238
anishazahan <anishazahan13@gmail.com>
2023-09-09 16:26:34 +0600

    features products component design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+// import Categorys from "../components/Categorys";
+// import FeatureProducts from "../components/products/FeatureProducts";
 
 const Home = () => {
   return (
     <>
       <Header></Header>
       <Banner></Banner>
+      <div className="my-4">{/* <Categorys></Categorys> */}</div>
+      {/* <div className='py-[45px]'>
+                <FeatureProducts products={products} />
+            </div> */}
     </>
   );
 };
